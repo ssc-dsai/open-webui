a669923510a45c16fcc97a3442e0c0b4002855b4
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-10-07 14:04:06 -0700

    refac: knowledge file handling ui behaviour

--- src/lib/components/workspace/Knowledge/Collection.svelte
+++ src/lib/components/workspace/Knowledge/Collection.svelte
@@ -2,7 +2,6 @@
 	import Fuse from 'fuse.js';
 	import { toast } from 'svelte-sonner';
 	import { v4 as uuidv4 } from 'uuid';
-	import { PaneGroup, Pane, PaneResizer } from 'paneforge';
 
 	import { onMount, getContext, onDestroy, tick } from 'svelte';
 	const i18n = getContext('i18n');
