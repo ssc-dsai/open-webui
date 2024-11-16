b5fdff10902f6ed4553befd0debb71380ceff8a2
Timothy Jaeryang Baek <tim@openwebui.com>
2024-11-16 14:52:07 -0800

    refac

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -1038,7 +1038,6 @@ app.mount("/api/v1", webui_app)
 webui_app.state.EMBEDDING_FUNCTION = retrieval_app.state.EMBEDDING_FUNCTION
 
 
-@cached(ttl=1)
 async def get_all_base_models():
     open_webui_models = []
     openai_models = []
